9b8a91e2a8d20de949b9d106851537e468007f4e
norr0065 <norr0065@algonquinlive.com>
2015-03-04 14:25:45 -0500

    Styled list font

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $form.on('submit', function (e) {
     e.preventDefault();
     
     var $li = $('<li>').html($name.val());
-    var $nameDl = $('<button class="delete">X</button>');
+    var $nameDl = $('<button class="delete">x</button>');
 
 $nameDl.on('click', function () {
         $li.remove('li');
